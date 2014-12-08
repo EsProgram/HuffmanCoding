83eeb1229483a9b871e161cdb788c1df071a0613
Es_Program <radwimps.september@gmail.com>
2014-12-08 21:08:17 +0900

    first cimmit

---
+++ HuffmanCoding/Node.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HuffmanCoding
+{
+    internal class Node : DynamicObject
+    {
+        private Dictionary<string, object> dynamic_field;//scalable element
+        private Node left;
+        private Node right;
+
+        public char Element { get; private set; }//normal element
+
+        public dynamic Left
+        {
+            get { return (dynamic)left; }
+            set { left = value; }
+        }
+
+        public dynamic Right
+        {
+            get { return (dynamic)right; }
+            set { right = value; }
+        }
+
+        public Node(char code, Node left = null, Node right = null)
+            : base()
+        {
+            dynamic_field = new Dictionary<string, object>();
+
+            Element = code;
+            Left = left;
+            Right = right;
+        }
+
+        public override bool TryGetMember(GetMemberBinder binder, out object result)
+        {
+            if(dynamic_field.ContainsKey(binder.Name))
+            {
+                result = dynamic_field[binder.Name];
+                return true;
+            }
+            else
+            {
+                result = null;
+                return false;
+            }
+        }
+
+        public override bool TrySetMember(SetMemberBinder binder, object value)
+        {
+            dynamic_field[binder.Name] = value;
+            return true;
+        }
+    }
+}
---
+++ HuffmanCoding/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HuffmanCoding
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Node root = new Node('a');
+            Node el1 = new Node('b');
+            root.Left = el1;
+
+            Console.WriteLine(root.Left.Element);
+        }
+    }
+}
