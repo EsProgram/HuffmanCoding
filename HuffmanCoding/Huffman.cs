afed805eed5ea266b7263711f40bd77be337448a
Es_Program <radwimps.september@gmail.com>
2014-12-09 01:07:31 +0900

    update

--- HuffmanCoding/Huffman.cs
+++ HuffmanCoding/Huffman.cs
@@ -14,6 +14,7 @@ namespace HuffmanCoding
     {
         private Dictionary<TNodeElement, float> data;
         private List<dynamic> nodes;
+        private dynamic root;
 
         /// <summary>
         /// データからハフマン符号を生成する
@@ -37,6 +38,7 @@ namespace HuffmanCoding
             //リーフノードを降順ソート
             nodes.Sort((a, b) => a.prob.CompareTo(b.prob));
             //木の生成
+            root = CreateTree(nodes);
             //木生成時に出来たノードを拡張
             ////////////////////////////////////////////////
         }
@@ -52,6 +54,19 @@ namespace HuffmanCoding
                 nodes.Add(new Node<TNodeElement>(item.Key));
                 nodes[item.i].prob = item.Value;
             }
+        }
+
+        private dynamic CreateTree(List<dynamic> leafs)
+        {
+            Stack<dynamic> s = new Stack<dynamic>(leafs);
+            //ノードの数が1になったら終了
+            //出現確率の低い2つのノードをpop
+            //2つのノードの親を作成する
+            //親に新しいprob(出現確率)を与える
+            //親をpushする
+
+            //最後に残った木のルートを返す
+            return null;
         }
     }
 }
